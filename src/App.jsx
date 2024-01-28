a2a7c04409ce465ebac04d10016b7919d03153c3
joadelvia <joadelvia@gmail.com>
2024-01-28 20:27:03 +0100

    Pequeño detalle página inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import Issues from "./components/Issues";
@@ -11,6 +11,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/home" replace />} />
           <Route path="home" element={<Home />}></Route>
           <Route path="issues" element={<Issues />}></Route>
           <Route path="issues/add" element={<IssuesForm />}></Route>
